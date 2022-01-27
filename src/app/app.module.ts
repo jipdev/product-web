cfe0de05fae111bdf6a6e43e63eaca6d4fd59924
Jhonattan Loli Mazzuco <jhonattanloli@gmail.com>
2022-01-27 19:21:05 +0044

    Alterado o locale para ptbr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { registerLocaleData } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from '@angular/core';
+import localePt from '@angular/common/locales/pt'
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMaskModule } from "ngx-mask";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShellModule } from "./core/shell/shell.module";
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -19,7 +23,18 @@ import { ShellModule } from "./core/shell/shell.module";
     ShellModule,
     NgxMaskModule.forRoot()
   ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "pt"
+    },
+    {
+      provide:  DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
+
